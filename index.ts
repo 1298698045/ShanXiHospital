e91c59094634d80f4e0cff7cfdca3b3bd2511634
zd <1298698045@qq.com>
2021-01-12 19:39:08 +0800

    山西生中医院官网首页

---
+++ index.ts
@@ -0,0 +1,15 @@
+var $: any;
+$(() => {
+    function onMyLoad():void{
+        let lis: any = $('.nav ul li');
+        for(var i = 0; i < lis.length; i++){
+            let num:number = i;
+            lis[num].onclick = function(){
+                // alert(num);
+                $(this).addClass('active').siblings().removeClass('active');
+                $(this).children("span").addClass('active').parent().siblings().children("span").removeClass('active');
+            }
+        }
+   }
+   onMyLoad();
+})
---
+++ js/index.js
@@ -0,0 +1,16 @@
+"use strict";
+var $;
+$(function () {
+    function onMyLoad() {
+        var lis = $('.nav ul li');
+        for (var i = 0; i < lis.length; i++) {
+            var num = i;
+            lis[num].onclick = function () {
+                // alert(num);
+                $(this).addClass('active').siblings().removeClass('active');
+                $(this).children("span").addClass('active').parent().siblings().children("span").removeClass('active');
+            };
+        }
+    }
+    onMyLoad();
+});
