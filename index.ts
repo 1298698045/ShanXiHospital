61949f997d639c3752b70575e5d31c00e0044755
zd <1298698045@qq.com>
2021-01-14 18:37:07 +0800

    首页提交

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
 var $: any;
 $(() => {
+    // $('.lis_p').click(function () { 
+    //     $('.lis_p').removeClass('active')
+    //     $(this).addClass('active');
+    //     $(this).parents('.content').removeClass('active');
+    // })
     function onMyLoad():void{
         let lis: any = $('.nav ul li');
         for(var i = 0; i < lis.length; i++){
@@ -8,6 +13,7 @@ $(() => {
                 // alert(num);
                 // $(this).addClass('active').siblings().removeClass('active');
                 $(this).children("span").addClass('active').parent().siblings().children("span").removeClass('active');
+                $(this).children(".content").addClass('active').parent().siblings().children(".content").removeClass('active');
             }
         }
    }
--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 "use strict";
 var $;
 $(function () {
+    // $('.lis_p').click(function () { 
+    //     $('.lis_p').removeClass('active')
+    //     $(this).addClass('active');
+    //     $(this).parents('.content').removeClass('active');
+    // })
     function onMyLoad() {
         var lis = $('.nav ul li');
         for (var i = 0; i < lis.length; i++) {
@@ -9,6 +14,7 @@ $(function () {
                 // alert(num);
                 // $(this).addClass('active').siblings().removeClass('active');
                 $(this).children("span").addClass('active').parent().siblings().children("span").removeClass('active');
+                $(this).children(".content").addClass('active').parent().siblings().children(".content").removeClass('active');
             };
         }
     }
