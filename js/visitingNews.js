7ddc6ba23ae68d25504082d2996ee35776e1c935
zd <1298698045@qq.com>
2021-06-21 20:47:14 +0800

    出诊查询添加数据、修改部分跳转链接、历史沿革数据

--- js/visitingNews.js
+++ js/visitingNews.js
@@ -94,14 +94,16 @@ $(function(){
             name:"刘丽坤",
             title:"主任医师、省优秀专家、省名医",
             indications:"肺癌；乳腺癌；结肠直肠癌",
-            time:"周5上午"
+            time:"周5上午",
+            rowspan:1
         },
         {
             departName:"脾胃科",
             name:"冯五金",
             title:"主任医师、享受国贴专家、省名医、省优秀专家",
             indications:"慢性胃炎、慢性结肠炎、 消化不良、溃疡性结肠炎",
-            time:"周5上午"
+            time:"周5上午",
+            rowspan:5
         },
         {
             departName:"脾胃科",
@@ -138,7 +140,8 @@ $(function(){
             name:"米彩云",
             title:"主任医师、省优秀专家、省名医",
             indications:"急慢肾炎，肾功衰竭、IgA肾病，肾炎血尿、复发性尿路感染",
-            time:"周1上午"
+            time:"周1上午",
+            rowspan:2
         },
         {
             departName:"肾病一科",
@@ -153,7 +156,8 @@ $(function(){
             name:"宋跃飞",
             title:"主任医师",
             indications:"各种肾脏疾病、前列腺疾病、内科杂病",
-            time:"周3上午"
+            time:"周3上午",
+            rowspan:1
         },
 
         {
@@ -161,7 +165,8 @@ $(function(){
             name:"吴艳萍",
             title:"主任医师",
             indications:"盆腔炎、月经失调、外阴营养不良",
-            time:"周2、3全天，周1、5下午"
+            time:"周2、3全天，周1、5下午",
+            rowspan:5
         },
         {
             departName:"妇科",
@@ -197,7 +202,8 @@ $(function(){
             name:"曹月英",
             title:"主任医师、享受国贴专家、省名老中医、省优秀专家 ",
             indications:"各种急慢性肝炎、肝硬化及并发症、肝脏肿瘤",
-            time:"周3上午"
+            time:"周3上午",
+            rowspan:1
         },
 
         {
@@ -205,7 +211,8 @@ $(function(){
             name:"牛天福",
             title:"主任医师省、优秀专家、省名医",
             indications:"冠心病、高血压病、心律失常",
-            time:"周3下午"
+            time:"周3下午",
+            rowspan:2
         },
         {
             departName:"心病科",
@@ -220,7 +227,8 @@ $(function(){
             name:"焦黎明",
             title:"主任医师、省优秀专家、院名医",
             indications:"脑血管疾病、头痛眩晕面瘫、失眠焦虑抑郁",
-            time:"周4上午"
+            time:"周4上午",
+            rowspan:3
         },
         {
             departName:"脑病科",
@@ -242,7 +250,8 @@ $(function(){
             name:"王健",
             title:"主任医师",
             indications:"眼表疾病、外眼疾病、眼底疾病",
-            time:"周3上午"
+            time:"周3上午",
+            rowspan:1
         },
 
         {
@@ -250,7 +259,8 @@ $(function(){
             name:"李庭凯",
             title:"主任医师",
             indications:"咳嗽、汗症、调理体质、治未病",
-            time:"周2上午"
+            time:"周2上午",
+            rowspan:7
         },
         {
             departName:"内科",
@@ -300,7 +310,8 @@ $(function(){
             name:"郑世江",
             title:"主任医师",
             indications:"痛风、疱疹神经痛、颈肩腰腿疼痛",
-            time:"周2、4上午，周5全天"
+            time:"周2、4上午，周5全天",
+            rowspan:2
         },
         {
             departName:"疼痛科",
@@ -315,7 +326,8 @@ $(function(){
             name:"李凤仙",
             title:"主任医师、省优秀专家、省名医",
             indications:"痤疮、牛皮癣、各种皮肤病",
-            time:"周4上午"
+            time:"周4上午",
+            rowspan:4
         },
         {
             departName:"皮肤科",
@@ -344,7 +356,8 @@ $(function(){
             name:"张传稷",
             title:"主任医师",
             indications:"四肢骨折脱位、骨关节炎、颈腰椎间盘突出",
-            time:"周3下午,周2上午"
+            time:"周3下午,周2上午",
+            rowspan:4
         },
         {
             departName:"骨伤科",
@@ -372,14 +385,16 @@ $(function(){
             name:"赵聚峰",
             title:"主任医师",
             indications:"颈椎病及腰椎病、骨质疏松压缩骨折、癌症椎体骨转移",
-            time:"周3上午"
+            time:"周3上午",
+            rowspan:1
         },
         {
             departName:"儿科",
             name:"李兰芳",
             title:"主任医师",
             indications:"小儿多动、秽语抽动症、目眨、小儿脾虚、消化不良、咳嗽",
-            time:"周6上午"
+            time:"周6上午",
+            rowspan:3
         },
         {
             departName:"儿科",
@@ -400,7 +415,8 @@ $(function(){
             name:"李建仲",
             title:"主任医师、省优秀专家、省名医",
             indications:"颈肩腰腿痛;头痛眩晕;面瘫耳鸣",
-            time:"周2下午"
+            time:"周2下午",
+            rowspan:5
         },
         {
             departName:"针灸推拿",
@@ -435,7 +451,8 @@ $(function(){
             name:"郑建明",
             title:"主治医师",
             indications:"糖尿病、高血压、内科疾病、湿疹、痤疮",
-            time:"周2、3、4、5上午"
+            time:"周2、3、4、5上午",
+            rowspan:2
         },
         {
             departName:"社区医师",
@@ -447,10 +464,18 @@ $(function(){
     ]
     function peaceFun(){
         let str = '';
+        var lastDeptName="";
+        var currentDeptName="";
         for(var i = 0 ; i < peaceData.length; i++){
-            str += `
-            <tr class="dataRow">
-                <td  class="name">${peaceData[i].departName}</td>
+            str += `<tr class="dataRow">`;
+            currentDeptName=peaceData[i].departName;
+           // debugger;
+            if(currentDeptName!=lastDeptName)
+            {
+                str += `<td class="name" rowspan="${peaceData[i].rowspan}">${currentDeptName}</td>`
+            }
+            
+            str += ` 
                 <td>${peaceData[i].name}</td>
                 <td>${peaceData[i].title}</td>
                 <td>${peaceData[i].indications}</td>
@@ -460,9 +485,540 @@ $(function(){
                     <button class="btn msg">在线咨询</button>
                 </td>
             </tr>
-            `
+            `;
+            lastDeptName= currentDeptName ;
         }
         $('#tbodyPeace').append(str);
+    }
+    let victoryData = [
+        {
+            departName:"肿瘤科",
+            name:"高向军",
+            title:"副主任医师",
+            indications:"内科杂病、各种肿瘤、消化系统疾病、失眠等",
+            time:"周2，5上午",
+            rowspan:3
+        },
+        {
+            departName:"肿瘤科",
+            name:"李宜放",
+            title:"主任医师",
+            indications:"消化道肿瘤、肺癌及并发症、泌尿与生殖系统肿瘤",
+            time:"周3上午"
+        },{
+            departName:"肿瘤科",
+            name:"南晓红",
+            title:"副主任医师",
+            indications:"各种肿瘤、胃肠道疾病、内科杂病",
+            time:"周1上午"
+        },
+        
+        
+        {
+            departName:"脾胃病科",
+            name:"肖芳",
+            title:"主任医师",
+            indications:"肠胃疾病、萎缩性胃炎、胰腺炎、胆囊炎、胆结石",
+            time:"周5上午",
+            rowspan:4
+        },
+        {
+            departName:"脾胃病科",
+            name:"李晶",
+            title:"主任医师",
+            indications:"慢性胃炎、慢性结肠炎、消化不良、溃疡性结肠炎",
+            time:"周2上午"
+        },
+        {
+            departName:"脾胃病科",
+            name:"杨丽萍",
+            title:"副主任医师",
+            indications:"消化系统疾病、功能性消化不良、慢性萎缩性胃炎",
+            time:"周4上午"
+        },
+        {
+            departName:"脾胃病科",
+            name:"胡江涛",
+            title:"主治医师",
+            indications:"内镜微创诊疗、消化不良、便秘",
+            time:"周1上午"
+        },
+        
+        
+        {
+            departName:"肾病一科",
+            name:"米彩云",
+            title:"主任医师、省优秀专家、省名老中医、院老年名医",
+            indications:"急性肾炎、肾功衰竭、IgA肾病、肾炎血尿、复发性尿路感染",
+            time:"周5上午",
+            rowspan:5
+        },
+        {
+            departName:"肾病一科",
+            name:"霍亚平",
+            title:"主任医师",
+            indications:"慢性肾病、泌尿系炎症、紫癜性肾炎",
+            time:"周4上午"
+        },
+        {
+            departName:"肾病一科",
+            name:"刘红",
+            title:"副主任医师",
+            indications:"各种肾脏疾病、急慢性肾衰竭、泌尿系疾病",
+            time:"周1上午"
+        },
+        {
+            departName:"肾病一科",
+            name:"李智",
+            title:"副主任医师",
+            indications:"紫癜肾、各种肾脏病、泌尿系感染",
+            time:"周4下午"
+        },
+        {
+            departName:"肾病一科",
+            name:"傅志慧",
+            title:"主治医师",
+            indications:"慢性肾脏疾病，泌尿系感染",
+            time:"周5下午"
+        },
+        
+        
+        {
+            departName:"肾病二科",
+            name:"王世荣",
+            title:"主任医师、国家优秀人才",
+            indications:"肾脏疾病、尿路感染、内科杂病",
+            time:"周2上午",
+            rowspan:3
+        },
+        {
+            departName:"肾病二科",
+            name:"韩履祺",
+            title:"主任医师、省名医、院老年名医",
+            indications:"各种肾脏病、泌尿系感染、内科疾病",
+            time:"周4下午"
+        },
+        {
+            departName:"肾病二科",
+            name:"钱雅玉",
+            title:"主任医师",
+            indications:"肾脏疾病，糖尿病肾病、尿路感染，水肿等内科疾病",
+            time:"周4上午"
+        },
+        
+        
+        
+        {
+            departName:"心血管科",
+            name:"牛天福",
+            title:"主任医师、省优秀专家、省名医",
+            indications:"冠心病、高血压病、心律失常",
+            time:"周4下午",
+            rowspan:1
+        },
+        
+        
+        {
+            departName:"妇科",
+            name:"郭凤荷",
+            title:"主任医师、省优秀专家、省名医",
+            indications:"外阴营养不良、慢性盆腔炎、月经失调",
+            time:"周4上午",
+            rowspan:4
+        },
+        {
+            departName:"妇科",
+            name:"王红",
+            title:"主任医师",
+            indications:"盆腔炎性疾病，妇科肿瘤，中医药调经，助孕，安胎",
+            time:"周1上午"
+        },
+        {
+            departName:"妇科",
+            name:"李瑛",
+            title:"主任医师",
+            indications:"不孕症（输卵管梗阻性不育）、异常子宫出血、盆腔炎",
+            time:"周5上午"
+        },
+        {
+            departName:"妇科",
+            name:"渠艳芳",
+            title:"主治医师",
+            indications:"经失调、盆腔炎性疾病、盆腔包块等、更年期综合症等妇科疾病",
+            time:"周1、2、3、4、5全天，周6上午"
+        },
+        
+        
+        
+        {
+            departName:"肝病科",
+            name:"李怀长",
+            title:"主任医师",
+            indications:"急慢性肝炎、肝硬化及其并发症、脂肪肝",
+            time:"周2上午",
+            rowspan:1
+        },
+        
+        
+        
+        {
+            departName:"内分泌科",
+            name:"冯玛莉",
+            title:"主任医师、享受国贴专家、省优秀专家",
+            indications:"糖尿病及并发症、痛风、黄褐斑",
+            time:"周6上午",
+            rowspan:1
+        },
+        
+        
+        
+        {
+            departName:"风湿病科",
+            name:"赵剑锋",
+            title:"副主任医师",
+            indications:"各种关节炎、干燥综合征、白塞病、免疫系统疾病、内科杂病",
+            time:"周3上午",
+            rowspan:3
+        },
+        {
+            departName:"风湿病科",
+            name:"周晓莉",
+            title:"副主任医师",
+            indications:"风湿类风湿关节炎、强直性脊柱炎、各种疼痛性疾病",
+            time:"周2上午"
+        },
+        {
+            departName:"风湿病科",
+            name:"张晓强",
+            title:"副主任医师",
+            indications:"各类风湿病、关节炎、结缔组织病；免疫失调，儿童风湿病",
+            time:"周4上午"
+        },
+        
+        
+        
+        {
+            departName:"脑病科",
+            name:"李小军",
+            title:"主任医师",
+            indications:"脑血管病、高血压，头痛眩晕，焦虑抑郁，失眠健忘，帕金森，面瘫",
+            time:"周4上午",
+            rowspan:3
+        },
+        {
+            departName:"脑病科",
+            name:"李晓东",
+            title:"副主任医师",
+            indications:"脑血管病；失眠焦虑抑郁症；头痛眩晕、面瘫",
+            time:"周5上午"
+        },
+        {
+            departName:"脑病科",
+            name:"卢国珍",
+            title:"主治医师",
+            indications:"脑血管疾病、头痛眩晕、失眠焦虑",
+            time:"周2下午"
+        },
+        
+        
+        {
+            departName:"肺病科",
+            name:"赵勤萍",
+            title:"主任医师",
+            indications:"慢性阻塞性肺疾病、支气管哮喘、双肺间质纤维化",
+            time:"周3上午",
+            rowspan:2
+        },
+        {
+            departName:"肺病科",
+            name:"关炜",
+            title:"副主任医师",
+            indications:"支气管哮喘及慢性阻塞性肺病、肺癌、双肺间质纤维化",
+            time:"周2上午"
+        },
+        
+        
+        {
+            departName:"内科",
+            name:"柏晋梅",
+            title:"主任医师、省优秀专家、省名医",
+            indications:"内科疾病、急慢性支气管炎、肺气肿",
+            time:"周6上午",
+            rowspan:12
+        },
+        {
+            departName:"内科",
+            name:"王祥云",
+            title:"主任医师",
+            indications:"急慢性支气管炎；高血压病；养生保健；内科杂病",
+            time:"周3下午"
+        },
+        {
+            departName:"内科",
+            name:"张恒",
+            title:"主任医师",
+            indications:"更年期综合征失眠；颈肩腰腿关节痛；肝胆泌尿系结石",
+            time:"周3上午"
+        },
+        {
+            departName:"内科",
+            name:"白平昌",
+            title:"主任医师",
+            indications:"急慢性肝炎、肝硬化、脂肪肝、消化系统疾患、呼吸系统疾患",
+            time:"周1、3上午"
+        },
+        {
+            departName:"内科",
+            name:"王琴",
+            title:"主任医师",
+            indications:"肾病、尿路感染，腰痛、紫癜、肾虚，脾胃病等内科杂病",
+            time:"周3上午"
+        },
+        {
+            departName:"内科",
+            name:"杨爱枫",
+            title:"副主任医师",
+            indications:"各种心血管疾病、内科疾病、更年期综合征",
+            time:"周2上午"
+        },
+        {
+            departName:"内科",
+            name:"肖振君",
+            title:"副主任医师",
+            indications:"内科疾病、浅表性胃炎、消化性溃疡病",
+            time:"周2上午"
+        },
+        {
+            departName:"内科",
+            name:"支开叶",
+            title:"副主任医师",
+            indications:"支气管哮喘、肺间质纤维化、内科疾病",
+            time:"周1上午"
+        },
+        {
+            departName:"内科",
+            name:"刘鹏",
+            title:"副主任医师",
+            indications:"慢性阻塞性肺疾病、支气管哮喘、慢性咳嗽及心脑血管疾病",
+            time:"周3下午"
+        },
+        {
+            departName:"内科",
+            name:"任海霞",
+            title:"主治医师",
+            indications:"糖尿病、高血压、甲状腺疾病及更年期，失眠等内科杂病",
+            time:"周1、2、3、4、5全天，周6上午"
+        },
+        {
+            departName:"内科",
+            name:"贾晶晶",
+            title:"主治医师",
+            indications:"咳嗽、咽炎、失眠、眩晕、心悸，胃痛腹胀等内科杂病",
+            time:"周1下午"
+        },
+        {
+            departName:"内科",
+            name:"李宏霞",
+            title:"主治医师",
+            indications:"过敏性鼻炎、咳嗽等肺部疾病；头晕等脑病；脾胃及内科杂病",
+            time:"周2下午"
+        },
+        
+        
+        
+        {
+            departName:"代谢病门诊",
+            name:"吕蕾",
+            title:"主任医师",
+            indications:"糖尿病并发症、甲状腺疾病、更年期综合症、内分泌紊乱等",
+            time:"周3下午"
+        },
+        {
+            departName:"代谢病门诊",
+            name:"董琦",
+            title:"主治医师",
+            indications:"内科疾病，糖尿病及其并发症，颈肩腰腿痛，面瘫",
+            time:"周4上午"
+        },
+        {
+            departName:"代谢病门诊",
+            name:"张维亮",
+            title:"主治医师",
+            indications:"内科疾病、糖尿病及其并发症、甲状腺疾病",
+            time:"周3上午"
+        },
+        
+        
+        
+        {
+            departName:"骨伤科",
+            name:"张传稷",
+            title:"主任医师",
+            indications:"四肢骨折脱位、骨关节炎、颈腰椎间盘突出",
+            time:"周5上午"
+        },
+        {
+            departName:"骨伤科",
+            name:"杜志伟",
+            title:"主任医师",
+            indications:"四肢骨折脱位、颈腰间盘突出症、老年性骨性关节炎",
+            time:"周4上午"
+        },
+        
+        
+        
+        {
+            departName:"骨关节科",
+            name:"刘小刚",
+            title:"主治医师",
+            indications:"颈肩腰腿痛、骨关节病、软组织损伤性疾病",
+            time:"周3下午"
+        },
+        
+        
+        
+        {
+            departName:"皮肤科",
+            name:"王建青",
+            title:"副主任医师、院青年名医",
+            indications:"成人及儿童皮肤病、杂病",
+            time:"周6上午"
+        },
+        {
+            departName:"皮肤科",
+            name:"梁晓军",
+            title:"主治医师",
+            indications:"皮肤病、胃肠疾病、失眠、鼻炎、更年期等杂病",
+            time:"周1、2、3、4、5全天，周6上午"
+        },
+        
+        
+        
+        {
+            departName:"（骨质疏松）脊柱外科",
+            name:"赵聚峰",
+            title:"主任医师",
+            indications:"颈椎病及腰椎病、骨质疏松压缩骨折、癌症椎体骨转移",
+            time:"周2下午"
+        },
+        
+        
+        
+        {
+            departName:"儿科",
+            name:"李兰芳",
+            title:"主任医师",
+            indications:"小儿多动、秽语抽动症、目眨、小儿脾虚、消化不良、咳嗽",
+            time:"周5下午"
+        },
+        {
+            departName:"儿科",
+            name:"王小芸",
+            title:"副主任医师",
+            indications:"反复呼吸道感染、小儿厌食、小儿遗尿",
+            time:"周5上午"
+        },
+        {
+            departName:"儿科",
+            name:"申聪",
+            title:"副主任医师",
+            indications:"小儿呼吸道疾病、常见消化系统疾病、小儿过敏性紫癜",
+            time:"周1、2、3、4、5全天，周6上午"
+        },
+        {
+            departName:"儿科",
+            name:"孟庆阔",
+            title:"主治医师",
+            indications:"咳嗽、过敏性鼻炎、多发性抽动症",
+            time:"周3上午"
+        },
+        
+        
+        
+        {
+            departName:"针灸科",
+            name:"陈猛",
+            title:"主治医师",
+            indications:"颈椎病、腰椎间盘突出症、膝关节炎、面瘫、中风后遗症",
+            time:"周1、2、3、4、5全天，周6上午"
+        },
+        {
+            departName:"针灸科",
+            name:"靳晓娟",
+            title:"主治医师",
+            indications:"颈肩腰腿痛、面瘫、失眠等内科常见病，小儿推拿",
+            time:"周1、2、3、4、5全天，周6上午"
+        },
+        
+        
+        
+        {
+            departName:"推拿科",
+            name:"王丽",
+            title:"副主任医师",
+            indications:"小儿推拿、颈椎病、腰椎间盘突出症、面瘫、痛经",
+            time:"周5上午"
+        },
+        {
+            departName:"推拿科",
+            name:"崔志敏",
+            title:"主治医师",
+            indications:"小儿推拿、颈肩腰腿痛、中风后遗症",
+            time:"周1、2、3、4、5全天，周6上午"
+        },
+        {
+            departName:"推拿科",
+            name:"田祥耐",
+            title:"医师",
+            indications:"小儿腺样体肥大、鼻炎、消化不良等儿科常见疾病、颈肩腰腿痛",
+            time:"周1、2、3、4、5全天，周6上午"
+        },
+        {
+            departName:"推拿科",
+            name:"贺志红",
+            title:"医师",
+            indications:"小儿常见疾病、成人颈肩腰腿痛、其他疼痛性疾病",
+            time:"周1、2、3、4、5全天，周6上午"
+        },
+        
+        
+        
+        
+        {
+            departName:"肝肠科",
+            name:"畅立强",
+            title:"副主任医师",
+            indications:"混合型便秘、结直肠良性、恶性肿瘤、混合痔及男性病",
+            time:"周3上午"
+        }
+    ]
+    function victoryFun(){
+        let str = '';
+        var lastDeptName="";
+        var currentDeptName="";
+        for(var i = 0 ; i < victoryData.length; i++){
+            str += `<tr class="dataRow">`;
+            currentDeptName=victoryData[i].departName;
+           // debugger;
+            if(currentDeptName!=lastDeptName)
+            {
+                str += `<td class="name" rowspan="${victoryData[i].rowspan}">${currentDeptName}</td>`
+            }
+            
+            str += ` 
+                <td>${victoryData[i].name}</td>
+                <td>${victoryData[i].title}</td>
+                <td>${victoryData[i].indications}</td>
+                <td>${victoryData[i].time}</td>
+                <td class="operation">
+                    <button class="btn">预约</button>
+                    <button class="btn msg">在线咨询</button>
+                </td>
+            </tr>
+            `;
+            lastDeptName= currentDeptName ;
+        }
+        $('#tbodyVictory').append(str);
     }
     $('#tab_child p').click(function(){
         $('#tab_child p').removeClass('active');
@@ -470,7 +1026,9 @@ $(function(){
         $('#visitInfo .blockWrap').hide();
         $('#visitInfo .blockWrap').eq($(this).index()).show();
         let idx = $(this).index();
-        if(idx==3){
+        if(idx==2){
+            victoryFun();
+        }else if(idx==3){
             peaceFun();
         }
     })
